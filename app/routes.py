1b287e76cceb88bff61b44094c544b24384dd63e
mkrajews <marcin.krajewski@intel.com>
2018-09-19 17:06:43 +0200

    Dodanie formularzy z modulu Flask-WTF
    - dodany forms.py (potzrebny do formularza logowania) ktory importuje moduł / tworzymu klasę LoginForm dziedziczącą po FlaskForm z modułu i nadajemy zmienne których będziemy używać
    - dodany template widoku login.html do którego wstawiamy pola z naszej klasy (flask sam wie jak mają wyglądać nie tzreba używać html)
    - dodany kod w routes.py:
       - importu klasy LoginForm z app->form
       - routowania z /login na login.html gdzie odpali funkcję login() która stworzy instancję naszej klasy form ze zmiennymi nastepnie wyrenderuje temlate z ich użyciem
    
    Chapter 3: Web Forms (this article) - Form Views

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')  # --> po wejsciu na ten adres
+def login():
+    form = LoginForm()  # -> stworz instancje 'form' klasy 'LoginForm' z form.py ->
+    return render_template('login.html', title='Sign In', form=form)  # -> wyrenderuj szablon login.html z wykorzystaniem danych z 'form'
