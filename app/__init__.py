4438a7d75e8efb75769b3fab7971a065dc9556fa
mkrajews <marcin.krajewski@intel.com>
2018-09-21 14:30:31 +0200

    Adding 2 extensions: flask-sqlalchemy & flask-migrate -> Configuration:
    app/__init__.py
    /config.py
    
    Chapter 4: Database - Flask-SQLAlchemy Configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)  # db - obiekt reprezentujący bazę danych
+migrate = Migrate(app, db)  # obiekt reprezentujący silnik migracji
 
+from app import routes, models  # models - moduł określi strukturę bazy danych
+
+
+# Sposób na odpalanie aplikacji
 # if __name__ == "__main__":
 #     app.run()
     # app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \  # Flask-SQLAlchemy pobiera lokalizację bazy danych aplikacji ze SQLALCHEMY_DATABASE_URIzmiennej konfiguracyjnej
+        'sqlite:///' + os.path.join(basedir, 'app.db')  # biorę adres URL bazy danych ze DATABASE_URLzmiennej środowiskowej, a jeśli to nie jest zdefiniowane, konfiguruję bazę danych o nazwie app.db znajdującą się w głównym katalogu aplikacji, która jest przechowywana w basedirzmiennej
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
