eb82257c98f90151ff620c377edd16e2052f8973
mkrajews <marcin.krajewski@intel.com>
2018-09-26 13:05:52 +0200

    Chapter 5: User Logins -> Requiring Users To Login -> Showing The Logged In User in Templates
    Działa logowanie
    Działa wymuszenie logowania
    Działa przekierowanie na ostatnio ogladaną stronę przed wymuszeniem zalogowania
    Pokazuje zalogowanego -> 1 user w db->  user: susan, pass: cat

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -19,7 +19,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])  # --> po wejsciu na /login uruchom i dodatkowo akceptuj GET i POST a nie tylko domyslne GET
 def login():
@@ -32,7 +32,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')  # Przechwycenie argumentu next strony z jakiej chcial skozystac niezalogowany user
+        if not next_page or url_parse(next_page).netloc != '':  # if nie istnieje next_page lub analizujac next_page za pomoca url_parse() i sprawdzajac czy netloc jest ustawiony wykazala ze cos bylo wstawione (dzieki netloc moznabylo zaszyc jakis adres/przekirowanie (hacker/security))
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)  # -> wyrenderuj szablon login.html z wykorzystaniem danych z 'form'
 
 @app.route('/logout')
