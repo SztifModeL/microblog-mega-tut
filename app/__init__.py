1e9245ccc063581716875b0de9ec93d817f3684e
mkrajews <marcin.krajewski@intel.com>
2018-10-05 22:23:07 +0200

    2.02 -> Chapter 7: Sending Errors by Email -> +Wysylanie bledow przez e-mail
    +import logging z obsługa SMTP +warunkowa obsluga wysylania e-maili -> /app/__init__.py
    +konfiguracja servera SMTP -> /config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -13,6 +15,22 @@ migrate = Migrate(app, db)  # obiekt reprezentujacy silnik migracji
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+if not app.debug:  # jesli nie jest w trybie debug
+    if app.config['MAIL_SERVER']:  # i jesli skonfigurowany jest server poczty
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)  # ustawia poziom raportowania tylko na bledy
+        app.logger.addHandler(mail_handler)  # dolacza SMTPHandler do obiektu Flask
 
 from app import routes, models, errors  # models - modul okresli strukture bazy danych
 
--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')  # Flask-SQLAlchemy pobiera lokalizacje bazy danych aplikacji ze SQLALCHEMY_DATABASE_URIzmiennej konfiguracyjnej, biore adres URL bazy danych ze DATABASE_URL zmiennej srodowiskowej, a jesli to nie jest zdefiniowane, konfiguruje baze danych o nazwie app.db znajdujaca sie w glownym katalogu aplikacji, ktora jest przechowywana w basedir zmiennej
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['modeltest@wp.pl']
